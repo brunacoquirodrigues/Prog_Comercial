495c640757cf8d3bfabb0c3e94603595f16300f0
brunacoquirodrigues <Bruna.c@hotmail.com>
2018-06-01 12:26:46 -0300

    adicionando a app core
    app responsavel pelo crude do usuario

--- carRent/core/admin.py
+++ carRent/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from rental.models import Carro, Marca, Cor, Informacao, Imagem
+from rental.models import Aluguel
+from core.models import Usuario
 # Register your models here.
+
+admin.site.register((Carro,Marca,Cor,Informacao,Imagem))
+admin.site.register((Usuario))
--- carRent/core/models.py
+++ carRent/core/models.py
@@ -1,6 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 '''
     APP pra trabalhar com os Usuarios
 '''
+SEXO = (
+        (u'M', u'Masculino'),
+        (u'F', u'Feminino'),
+)
+
+class Usuario(models.Model):
+    """Model definition for Usuario."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cpf = models.CharField(max_length=11, null=False, unique=True)
+    sexo = models.CharField(max_length=1, null=False, choices=SEXO)
+    #profile_pic
+
+    class Meta:
+        """Meta definition for Usuario."""
+
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    def __str__(self):
+        """Unicode representation of Usuario."""
+        return 
