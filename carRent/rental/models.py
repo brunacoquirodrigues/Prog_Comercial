5fad9d7eaaa6787e9a7ff67e10dfff5d8321d816
brunacoquirodrigues <Bruna.c@hotmail.com>
2018-06-01 12:27:15 -0300

    adequacoes feita no banco
    algumas foreign keys estavam erradas

--- carRent/rental/models.py
+++ carRent/rental/models.py
@@ -1,82 +1,70 @@
 from django.db import models
 from datetime import datetime as tempo
 from django.core.validators import MinValueValidator, MaxValueValidator
+from core.models import Usuario
 
 # Create your models here.
 
-class Carro(models.Model):
-    """Model definition for Carro."""
-    nome = models.CharField(max_length=30, blank=False, null=False)
-    ano  = models.PositiveIntegerField(
-            validators=[
-                MinValueValidator(1900), 
-                MaxValueValidator(tempo.now().year)],
-            help_text="Use the following format: <YYYY>")
-
-    alugado = models.BooleanField(blank=False, null=False)
-
-    class Meta:
-        """Meta definition for Carro."""
-        verbose_name = 'Carro'
-        verbose_name_plural = 'Carros'
-
-    def __str__(self):
-        """Unicode representation of Carro."""
-        return "{} {}".format(self.nome, self.ano)
-
-    # TODO: Define custom methods here
-
 class Cor(models.Model):
-    """Model definition for Cor."""
-    carro = models.OneToOneField(Carro, on_delete=models.CASCADE, primary_key = True)
     cor = models.CharField(max_length=30, blank=False, null=False)
 
     class Meta:
-        """Meta definition for Cor."""
-
         verbose_name = 'Cor'
         verbose_name_plural = 'Cores'
 
     def __str__(self):
-        """Unicode representation of Cor."""
-        return "cor:%s do carro %s" %s(self.cor, self.carro.nome)
+        return "%s" %(self.cor)
 
 class Marca(models.Model):
-    """Model definition for Marca."""
-    carro = models.OneToOneField(Carro, on_delete=models.CASCADE, primary_key = True)
     marca = models.CharField(max_length=30, blank=False, null=False)
 
     class Meta:
-        """Meta definition for Marca."""
-
         verbose_name = 'Marca'
         verbose_name_plural = 'Marcas'
 
     def __str__(self):
-        """Unicode representation of Marca."""
-        return "carro: %s de marca: %s" %(self.carro.nome, self.marca)
+        return "%s" %(self.marca)
+
+class Carro(models.Model):
+    modelo = models.CharField(max_length=30, blank=False, null=False)
+    ano  = models.PositiveIntegerField(
+            validators=[
+                MinValueValidator(1900), 
+                MaxValueValidator(tempo.now().year)],
+            help_text="Use o formato: AAAA")
+    cor_carro_fk = models.ForeignKey(Cor, on_delete=models.CASCADE)
+    marca_carro_fk = models.ForeignKey(Marca, on_delete=models.CASCADE, default=0)
+    #placa unique
+    alugado = models.BooleanField(blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Carro'
+        verbose_name_plural = 'Carros'
+
+    def __str__(self):
+        return "Marca: %s -> Modelo: %s -> Ano: %s -> Alugado:%s" %(self.marca_carro_fk.marca, self.modelo, self.ano, self.alugado)
+
+    # TODO: Define custom methods here
+
 
 class Informacao(models.Model):
-    """Model definition for Informacao."""
-    carro = models.OneToOneField(Carro, on_delete=models.CASCADE, primary_key=True)
+    info_carro = models.OneToOneField(Carro, on_delete=models.CASCADE, primary_key=True)
     tempo_de_uso = models.PositiveIntegerField(blank=False, null=False)
     km_rodados = models.PositiveIntegerField(blank=False, null=False)
     descricao_carro = models.TextField(blank=False, null=False, max_length=300)
 
     class Meta:
-        """Meta definition for Informacao."""
-
         verbose_name = 'Informação'
         verbose_name_plural = 'Informações'
 
     def __str__(self):
-        """Unicode representation of Informacao."""
         pass
 
 class Aluguel(models.Model):
     """Model definition for Aluguel."""
     # usuario
-    carro = models.OneToOneField(Carro, on_delete=models.CASCADE, primary_key = True)
+    usuario_fk = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='aluguel')
+    aluguel_carro = models.OneToOneField(Carro, on_delete=models.CASCADE, primary_key = True)
     data_inicio = models.DateTimeField(auto_now_add=True)
     data_fim = models.DateTimeField(auto_now=True)
     km_rodadps = models.PositiveIntegerField()
@@ -89,4 +77,20 @@ class Aluguel(models.Model):
 
     def __str__(self):
         """Unicode representation of Aluguel."""
-        pass
+        return "carro: %s alugado em: %s ate: %s" %(self.aluguel_carro.modelo, self.data_inicio, self.data_fim)
+
+class Imagem(models.Model):
+    """Model definition for Imagem."""
+
+    carro_imagem = models.ForeignKey(Carro, on_delete=models.CASCADE, related_name='imagens')
+    #imagem_file
+
+    class Meta:
+        """Meta definition for Imagem."""
+
+        verbose_name = 'Imagem'
+        verbose_name_plural = 'Imagens'
+
+    def __str__(self):
+        """Unicode representation of Imagem."""
+        return "Imagem do carro: %s" %(self.carro_imagem.primary_key)
