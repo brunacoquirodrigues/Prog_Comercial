5bb3d63df9923f9b01937bc026fb120dcb1fe895
brunacoquirodrigues <Bruna.c@hotmail.com>
2018-06-01 23:10:47 -0300

    Append no urlpattern += static

--- carRent/carRent/urls.py
+++ carRent/carRent/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 #from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
+
 
 urlpatterns = [
+    path('alugar/', TemplateView.as_view(template_name = "homepage.html")),
     path('admin/', admin.site.urls),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
-    path('',TemplateView.as_view(template_name='homepage.html')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
