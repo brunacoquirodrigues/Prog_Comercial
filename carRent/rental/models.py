590c6b22b92723ba446e8bd586d83ff762cd7d9c
brunacoquirodrigues <Bruna.c@hotmail.com>
2018-06-01 23:09:30 -0300

    Adequacoes nos models, relacionando ao usuario

--- carRent/rental/models.py
+++ carRent/rental/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from datetime import datetime as tempo
 from django.core.validators import MinValueValidator, MaxValueValidator
-from core.models import Usuario
-
-# Create your models here.
+from core.models import ProfileUsuario
 
 class Cor(models.Model):
     cor = models.CharField(max_length=30, blank=False, null=False)
@@ -44,9 +42,6 @@ class Carro(models.Model):
     def __str__(self):
         return "Marca: %s -> Modelo: %s -> Ano: %s -> Alugado:%s" %(self.marca_carro_fk.marca, self.modelo, self.ano, self.alugado)
 
-    # TODO: Define custom methods here
-
-
 class Informacao(models.Model):
     info_carro = models.OneToOneField(Carro, on_delete=models.CASCADE, primary_key=True)
     tempo_de_uso = models.PositiveIntegerField(blank=False, null=False)
@@ -61,36 +56,32 @@ class Informacao(models.Model):
         pass
 
 class Aluguel(models.Model):
-    """Model definition for Aluguel."""
-    # usuario
-    usuario_fk = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='aluguel')
+    usuario_fk = models.ForeignKey(ProfileUsuario, on_delete=models.CASCADE, related_name='aluguel')
     aluguel_carro = models.OneToOneField(Carro, on_delete=models.CASCADE, primary_key = True)
     data_inicio = models.DateTimeField(auto_now_add=True)
     data_fim = models.DateTimeField(auto_now=True)
-    km_rodadps = models.PositiveIntegerField()
+    km_rodados = models.PositiveIntegerField(null=True, blank=True)
 
     class Meta:
-        """Meta definition for Aluguel."""
-
         verbose_name = 'Aluguel'
         verbose_name_plural = 'Alugueis'
 
     def __str__(self):
-        """Unicode representation of Aluguel."""
         return "carro: %s alugado em: %s ate: %s" %(self.aluguel_carro.modelo, self.data_inicio, self.data_fim)
+    
+    def save(self):
+        if self.aluguel_carro.alugado == True:
+            return "Carro:%s ja alugado" %(self.aluguel_carro.modelo)
+        else:
+            super(Aluguel, self).save() 
 
 class Imagem(models.Model):
-    """Model definition for Imagem."""
-
     carro_imagem = models.ForeignKey(Carro, on_delete=models.CASCADE, related_name='imagens')
-    #imagem_file
+    profile_pic = models.ImageField(upload_to="carros/", blank=True, null=True)
 
     class Meta:
-        """Meta definition for Imagem."""
-
         verbose_name = 'Imagem'
         verbose_name_plural = 'Imagens'
 
     def __str__(self):
-        """Unicode representation of Imagem."""
         return "Imagem do carro: %s" %(self.carro_imagem.primary_key)
